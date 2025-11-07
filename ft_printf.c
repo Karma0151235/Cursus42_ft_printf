5d9ad065c15eec8c57aeba01a2d7d0c9fa8d416f
Stephen Tee <stee@u82z04s01.42kl.edu.my>
2025-11-07 16:43:19 +0800

    added some utils and prototyped logic

---
+++ ft_printf.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stee <stee@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 14:34:37 by stee              #+#    #+#             */
+/*   Updated: 2025/11/07 16:40:50 by stee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	ft_handle(const char flag, const va_list args)
+{
+	if (flag == 'c')
+		ft_putchar(va_arg(args, char));
+	if (flag == 's')
+		ft_putstr(va_arg(args, char *));
+	if (flag == 'i')
+		ft_putnbr(va_arg(args, int));
+	if (flag == 'd' || flag == 'u')
+		ft_putdouble(va_arg(args, double));
+	if (flag == '%')
+		ft_putchar('%');
+	if (flag == 'p')
+		ft_putptr(va_arg(args, void *));
+	if (flag == 'x' || flag == 'X')
+		ft_putstr(ft_tohexa(va_arg(args, int), flag));
+}
+
+int	ft_printf(const char *s, ...)
+{
+	int		i;
+	va_list	args;
+	va_start(args, s);
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '%' && s[i + 1] != '\0')
+		{
+			ft_handle(s[i + 1], args);
+			i++;
+		}
+		else
+			ft_putchar(s[i]);
+		i++;
+	}
+	va_end(list);
+	return (i);
+}
---
+++ ft_printf_utils.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stee <stee@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 16:12:28 by stee              #+#    #+#             */
+/*   Updated: 2025/11/07 16:40:27 by stee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char	*ft_tohexa(const int arg, const char flag)
+{
+	char	*base16;
+	char	int_str[32];
+	long	nbr;
+	int		i;
+
+	base16 = "0123456789abcdef";
+	nbr = (long) arg;
+	i = 1;
+	if (nbr < 0)
+	{
+		int_str[0] = '-';
+		nbr *= -1;
+	}
+	while (nbr > 0)
+	{
+		int_str[i] = base16[nbr % 16];
+		nbr /= 16;
+		i++;
+	}
+	if (flag == 'x')
+		return (int_str);
+	return (ft_toupper(int_str));
+}
+
